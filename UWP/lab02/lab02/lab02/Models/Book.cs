56ccc553c4b4c397a54f9f2d49afcc814f0aa4d5
thanhvinhhumg <thanhvinh200293@gmail.com>
2021-01-27 19:51:49 +0700

    lab02

---
+++ UWP/lab02/lab02/lab02/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+            books.Add(new Book { BookId = 1, Title="Vulpate 1",Author="Future 1", CoverImage="Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Vulpate 2", Author = "Future 2", CoverImage = "Assets/2.png" });
+            books.Add(new Book { BookId = 3, Title = "Vulpate 3", Author = "Future 3", CoverImage = "Assets/3.png" });
+            return books;
+        }
+    }
+}
